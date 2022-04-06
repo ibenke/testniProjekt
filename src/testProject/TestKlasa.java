65247df99acdcf5edef199d9318ee026f63e236e
Ivan <Ivan@192.168.1.10>
2022-04-06 20:28:56 +0200

    prvi commit

---
+++ src/testProject/TestKlasa.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class TestKlasa {
+
+	public static void main(String[] args) {
+        // ispis poruke
+        System.out.println("Testiranje instalacije je uspjesno!");
+	}
+
+}
